f0966f38a2b7a2dd26cb6409b5f5d59d0dc59eee
Siri Mykland <sirim_mykland@hotmail.com>
2020-02-20 15:53:33 +0100

    initial commit

---
+++ desktop/src/com/battleship/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.battleship.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.battleship.Battleship;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Battleship(), config);
+	}
+}
