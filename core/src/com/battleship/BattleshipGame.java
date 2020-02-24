dd39816045a418a5a4a8b6080ef6f1f7c57ef308
Siri Mykland <sirim_mykland@hotmail.com>
2020-02-24 14:20:49 +0100

    android and desktop config runs

--- core/src/com/battleship/BattleshipGame.java
+++ core/src/com/battleship/BattleshipGame.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Battleship extends ApplicationAdapter {
+public class BattleshipGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
--- desktop/src/com/battleship/desktop/DesktopLauncher.java
+++ desktop/src/com/battleship/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.battleship.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.battleship.Battleship;
+import com.battleship.BattleshipGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Battleship(), config);
+		new LwjglApplication(new BattleshipGame(), config);
 	}
 }
