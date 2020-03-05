20ef12e0843c1cfe3cb2ac9dfe71405c4465b2ab
henriklarsen <henrifla@stud.ntnu.no>
2020-03-05 09:58:55 +0100

    lint2

--- core/src/com/battleship/BattleshipGame.kt
+++ core/src/com/battleship/BattleshipGame.kt
@@ -7,10 +7,9 @@ import com.battleship.controller.state.PlayState
 
 class BattleshipGame : Game() {
 
-
     override fun create() {
         GameStateManager.push(PlayState())
-        var camera = OrthographicCamera(Gdx.graphics.width.toFloat(),  Gdx.graphics.height.toFloat())
+        var camera = OrthographicCamera(Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
         camera.setToOrtho(false, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
     }
 
