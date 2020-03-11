ed11acb25cf58aced9926530dd3b1a3c331fc854
Mats Tyldum <ma.tyldum@gmail.com>
2020-03-11 14:19:54 +0100

    Update HelpState.kt

--- core/src/com/battleship/controller/state/HelpState.kt
+++ core/src/com/battleship/controller/state/HelpState.kt
@@ -15,10 +15,9 @@ class HelpState : MenuState() {
     // TODO: Remove firebaseController here when it is not included in State anymore
     override var firebaseController: FirebaseController = FindPlayer()
 
-    override val buttons: List<Button> = listOf(TextButton(Gdx.graphics.width / 2 - 150f, Gdx.graphics.height - 200f, 300f, 150f, "Matchmaking") { GameStateManager.set(MatchmakingState()) },
-        TextButton(Gdx.graphics.width / 2 - 150f, Gdx.graphics.height - 800f, 300f, 150f, "Testmenu") { GameStateManager.set(TestMenuState()) },
-        TextButton(Gdx.graphics.width / 2 - 150f, Gdx.graphics.height - 400f, 300f, 150f, "Settings") { GameStateManager.set(SettingsState()) },
-        TextButton(Gdx.graphics.width / 2 - 150f, Gdx.graphics.height - 600f, 300f, 150f, "Play") { GameStateManager.set(PlayState()) })
+    override val buttons: List<Button> = listOf(
+        TextButton(Gdx.graphics.width / 2 - 150f, Gdx.graphics.height - 200f, 300f, 150f, "Help menu") { GameStateManager.set(MatchmakingState()) }
+    )
 
     override fun update(dt: Float) {
     }
