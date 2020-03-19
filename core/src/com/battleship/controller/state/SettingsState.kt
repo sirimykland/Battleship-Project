ac4b23733850a541780c9cf0f6a2df614db213fc
Mats Tyldum <ma.tyldum@gmail.com>
2020-03-19 16:56:20 +0100

    Update SettingsState.kt

--- core/src/com/battleship/controller/state/SettingsState.kt
+++ core/src/com/battleship/controller/state/SettingsState.kt
@@ -1,6 +1,7 @@
 package com.battleship.controller.state
 
 import com.badlogic.gdx.Gdx
+import com.battleship.BattleshipGame
 import com.battleship.GameStateManager
 import com.battleship.model.ui.GuiObject
 import com.battleship.utility.Font
@@ -10,27 +11,35 @@ import com.battleship.view.BasicView
 import com.battleship.view.View
 
 class SettingsState : GuiState() {
-
-    private val settingsList = listOf(
-        Pair("Sound on") { println("Sound on") },
-        Pair("Help") { GameStateManager.set(HelpState()) }
-    )
-
     override val guiObjects: List<GuiObject> = listOf(
-        *settingsList.mapIndexed { i, (name, func) ->
-            GUI.textButton(
-                Gdx.graphics.width / 2 - 170f,
-                Gdx.graphics.height - (380f + 180f * i),
-                340f,
-                140f,
-                name,
-                font = Font.MEDIUM_WHITE,
-                color = Palette.BLACK,
-                borderColor = Palette.BLUE,
-                onClick = func
-            )
-        }.toTypedArray(),
-
+        GUI.textButton(
+            Gdx.graphics.width / 2 - 170f,
+            Gdx.graphics.height - 380f,
+            340f,
+            140f,
+            "Sound on/off",
+            font = Font.MEDIUM_WHITE,
+            color = Palette.BLACK,
+            borderColor = Palette.BLUE,
+            onClick =  {
+                print("Sound on/off")
+                if(BattleshipGame.music?.isPlaying == true)
+                    BattleshipGame.music?.pause()
+                else
+                    BattleshipGame.music?.play()
+            }
+        ),
+        GUI.textButton(
+            Gdx.graphics.width / 2 - 170f,
+            Gdx.graphics.height - 560f,
+            340f,
+            140f,
+            "Help",
+            font = Font.MEDIUM_WHITE,
+            color = Palette.BLACK,
+            borderColor = Palette.BLUE,
+            onClick = { GameStateManager.set(HelpState()) }
+        ),
         GUI.text(
             20f,
             Gdx.graphics.height - 220f,
