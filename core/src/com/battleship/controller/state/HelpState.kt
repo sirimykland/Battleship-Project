2263bc95fd860fa3cf286fc1193ec145c9239a6c
Mats Tyldum <ma.tyldum@gmail.com>
2020-03-19 11:59:34 +0100

    Update HelpState.kt

--- core/src/com/battleship/controller/state/HelpState.kt
+++ core/src/com/battleship/controller/state/HelpState.kt
@@ -2,24 +2,19 @@ package com.battleship.controller.state
 
 import com.badlogic.gdx.Gdx
 import com.battleship.GameStateManager
-import com.battleship.controller.firebase.FindPlayer
-import com.battleship.controller.firebase.FirebaseController
 import com.battleship.model.GameObject
-import com.battleship.model.ui.Button
-import com.battleship.model.ui.Header
+import com.battleship.model.ui.GuiObject
 import com.battleship.model.ui.Image
-import com.battleship.model.ui.TextBox
-import com.battleship.model.ui.TextButton
+import com.battleship.utility.Font
+import com.battleship.utility.GUI
 import com.battleship.view.BasicView
 import com.battleship.view.View
 
-class HelpState : MenuState() {
+class HelpState : GuiState() {
     override var view: View = BasicView()
-    // TODO: Remove firebaseController here when it is not included in State anymore
-    override var firebaseController: FirebaseController = FindPlayer()
 
-    private var currentPageIndex = 0
-    private val numberOfPages = 4 // Need to be equal to size of images/descriptions lists
+    private var pageIndex: Int = 0
+    private val numberOfPages: Int = 4 // Need to be equal to size of images/descriptions lists
 
     // TODO: Update this with real screenshots from the game when it is more completed
     private val images: List<Image> = listOf(
@@ -35,31 +30,66 @@ class HelpState : MenuState() {
         TextBox(20f, 150f, Gdx.graphics.width - 40f, 90f, "Third page"),
         TextBox(20f, 150f, Gdx.graphics.width - 40f, 90f, "Fourth page")
     )
+    private val nextPageButton = GUI.textButton(
+        Gdx.graphics.width - 150f,
+        30f,
+        100f,
+        90f,
+        "->"
+    ) {
+        pageIndex++
+        updateButtons()
+    }
+    private val previousPageButton = GUI.textButton(
+        30f,
+        30f,
+        100f,
+        70f,
+        "<-"
+    ) {
+        pageIndex--
+        updateButtons()
+    }.hide()
 
-    override var buttons: List<Button> = listOf(
-        TextButton(20f, Gdx.graphics.height - 110f, 150f, 90f, "Back") {
-            GameStateManager.set(MainMenuState())
-        },
-        TextButton(20f, 20f, 150f, 90f, "<--") {
-            if(currentPageIndex > 0) currentPageIndex -= 1
-        },
-        TextButton(Gdx.graphics.width - 170f, 20f, 150f, 90f, "-->") {
-            if (currentPageIndex < numberOfPages - 1) currentPageIndex += 1
-        }
+    override val guiObjects: List<GuiObject> = listOf(
+        GUI.text(
+            20f,
+            Gdx.graphics.height - 220f,
+            Gdx.graphics.width - 40f,
+            90f,
+            "Matchmaking",
+            Font.LARGE_WHITE
+        ),
+        nextPageButton,
+        previousPageButton,
+        *playerButtons,
+        GUI.backButton
     )
+    private fun updateButtons() {
+        
+    }
 
-    private var uiElements: Array<GameObject> = arrayOf()
+
 
     override fun update(dt: Float) {
-        uiElements = arrayOf(
+    }
+
+    override fun render() {
+        view.render(*guiObjects.toTypedArray())
+    }
+}
+
+    /*
+    private var uiElements: Array<GameObject> = arrayOf()
+    override fun update(dt: Float) {
+        /*uiElements = arrayOf(
             TextButton(20f, Gdx.graphics.height - 110f, 150f, 90f, "Back") { GameStateManager.set(MainMenuState()) },
             Header(Gdx.graphics.width / 2 - 150f, Gdx.graphics.height - 130f, 300f, 150f, "Help"),
             *buttons.toTypedArray()
         )
     }
-
     override fun render() {
-        when (currentPageIndex) {
+        /*when (currentPageIndex) {
             0 -> buttons = listOf(
                 TextButton(Gdx.graphics.width - 170f, 20f, 150f, 90f, "-->") {
                     currentPageIndex += 1
@@ -78,7 +108,19 @@ class HelpState : MenuState() {
                     currentPageIndex += 1
                 }
             )
-        }
-        view.render(*uiElements, images[currentPageIndex], descriptions[currentPageIndex])
+        }*/
+        view.render(*uiELeements.toTypedArray(), image, description)
     }
+
+    override var buttons: List<Button> = listOf(
+        TextButton(20f, Gdx.graphics.height - 110f, 150f, 90f, "Back") {
+            GameStateManager.set(MainMenuState())
+        },
+        TextButton(20f, 20f, 150f, 90f, "<--") {
+            if(currentPageIndex > 0) currentPageIndex -= 1
+        },
+        TextButton(Gdx.graphics.width - 170f, 20f, 150f, 90f, "-->") {
+            if (currentPageIndex < numberOfPages - 1) currentPageIndex += 1
+        }
+    )*/
 }
