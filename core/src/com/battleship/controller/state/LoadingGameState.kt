756dfa4c4419a256816d05b81bb368e5825679f7
Mats Tyldum <ma.tyldum@gmail.com>
2020-04-20 14:00:17 +0200

    Added GUI-dialog to loading state

--- core/src/com/battleship/controller/state/LoadingGameState.kt
+++ core/src/com/battleship/controller/state/LoadingGameState.kt
@@ -9,6 +9,8 @@ import com.battleship.view.View
 
 class LoadingGameState(private var controller: FirebaseController) : GuiState(controller) {
     override var view: View = PlayView()
+    var showDialog: Boolean = false
+    var opponentLeftRenders: Int = 0
 
     override fun create() {
         super.create()
@@ -17,9 +19,19 @@ class LoadingGameState(private var controller: FirebaseController) : GuiState(co
 
     private fun headerText(): String {
         if (GSM.activeGame!!.opponent.playerId == "") return "Waiting for opponent to join..."
-        else if (GSM.activeGame!!.opponent.board.treasures.isEmpty()) return "Waiting for opponent to register treasures..."
+        else if (GSM.activeGame!!.opponent.board.treasures.isEmpty())
+            return "Opponent joined! Waiting for ${GSM.activeGame!!.opponent.playerName} to register treasures..."
         return "null"
     }
+
+    private val opponentLeftDialog = GUI.dialog(
+        "${GSM.activeGame!!.opponent.playerName} left the game before registering treasures.",
+        listOf(Pair("Find new game", {
+            showDialog = false
+            // TODO: Add some controller logic here?
+            GSM.set(MatchmakingState(controller))
+        }))
+    )
 
     override val guiObjects: List<GuiObject> = listOf(
         GUI.header(headerText()),
@@ -31,18 +43,29 @@ class LoadingGameState(private var controller: FirebaseController) : GuiState(co
                 }
                 GSM.set(MainMenuState(controller))
             }
-        }
+        },
+        *opponentLeftDialog
     )
 
     override fun render() {
         this.view.render(*guiObjects.toTypedArray())
+
     }
 
     override fun update(dt: Float) {
         println("Opponent left the game: " + GSM.activeGame!!.opponentLeft)
-        if (GSM.activeGame!!.gameReady) {
-            GSM.set(PlayState(controller))
+
+        if (GSM.activeGame!!.opponentLeft) {
+            if (opponentLeftRenders < 2) opponentLeftRenders++
+
+            // First opponent left render, show GUI dialog
+            if (opponentLeftRenders == 1) showDialog = true
         }
+
+        if (showDialog) opponentLeftDialog.forEach { guiObject -> guiObject.show() }
+        else opponentLeftDialog.forEach { guiObject -> guiObject.hide() }
+
+        if (GSM.activeGame!!.gameReady) GSM.set(PlayState(controller))
     }
 
     override fun dispose() {
