f700cb0f99f3c6fd7b3496f154dcb3d922a5a82d
Siri Mykland <sirim_mykland@hotmail.com>
2020-04-20 12:44:34 +0200

    doc for treasure

--- core/src/com/battleship/model/treasures/Treasure.kt
+++ core/src/com/battleship/model/treasures/Treasure.kt
@@ -7,7 +7,14 @@ import com.badlogic.gdx.math.Vector2
 import com.battleship.BattleshipGame
 import com.battleship.model.GameObject
 
-abstract class Treasure(var position: Vector2, val rotate: Boolean) : GameObject() {
+/**
+ * Abstract class for treasures inheriting from [GameObject].
+ *
+ * @constructor:
+ * @property position: Vector2 - position of treasure on board grid
+ * @property rotate: Boolean - describes if treasure is rotated, default: false
+ */
+abstract class Treasure(var position: Vector2, private val rotate: Boolean = false) : GameObject() {
     abstract var dimension: Vector2
     abstract var name: String
     abstract var health: Int
@@ -17,6 +24,11 @@ abstract class Treasure(var position: Vector2, val rotate: Boolean) : GameObject
     private var padding = 3
     var revealed = false
 
+    /**
+     * Triggers a sound.
+     *
+     * @param volume: Float - the volume of the sound played
+     */
     fun playSound(volume: Float) {
         if (BattleshipGame.soundOn) {
             sound.stop()
@@ -24,10 +36,19 @@ abstract class Treasure(var position: Vector2, val rotate: Boolean) : GameObject
         }
     }
 
+    /**
+     * Types of Treasures
+     */
     enum class TreasureType {
         TREASURECHEST, GOLDCOIN, GOLDKEY
     }
 
+    /**
+     * Checks if a hit is valid
+     *
+     * @param coordinates: Vector2 - position on the grid
+     * @return boolean of valid hit
+     */
     fun hit(coordinates: Vector2): Boolean {
         for (i in 1 until dimension.x.toInt() + 1) {
             val x = position.x + i - 1
@@ -42,10 +63,18 @@ abstract class Treasure(var position: Vector2, val rotate: Boolean) : GameObject
         return false
     }
 
+    /**
+     * Decrement a treasure's health by 1
+     */
     fun takeDamage() {
         health--
     }
 
+    /**
+     * Wraps treasure into a map
+     * Map contains treasuretype, position and rotation boolean
+     * @return shipMap: Map<String, Any>
+     */
     fun toMap(): Map<String, Any> {
         val shipMap = mutableMapOf<String, Any>()
         shipMap["type"] = type.toString()
@@ -55,25 +84,45 @@ abstract class Treasure(var position: Vector2, val rotate: Boolean) : GameObject
         return shipMap
     }
 
+    /**
+     * @return boolean of whether treasure is found or not
+     */
     fun found(): Boolean {
         return health == 0
     }
 
+    /**
+     * Sets new position of the treasure
+     *
+     * @param pos: Vector2 - new position
+     */
     fun updatePosition(pos: Vector2) {
         position = pos
     }
 
+    /**
+     * Rotate treasure 90 degrees, by swapping dimention vector's x and y
+     */
     fun rotateTreasure() {
         val temp = dimension.x
         dimension.x = dimension.y
         dimension.y = temp
     }
 
-    override fun draw(batch: SpriteBatch, boardPos: Vector2, dimension: Vector2) {
+    /**
+     * Override method of [GameObject].
+     * draws treasure with texture, and position relative to provided position
+     * of the board.
+     *
+     * @param batch SpriteBatch - to draw text and images with
+     * @param position Vector2 - the position to start drawing
+     * @param dimension Vector2 - the size of the object to draw
+     */
+    override fun draw(batch: SpriteBatch, position: Vector2, dimension: Vector2) {
         if (found() || revealed) {
 
-            val xPos = boardPos.x + dimension.x * position.x + padding
-            val yPos = boardPos.y + dimension.y * position.y + padding
+            val xPos = position.x + dimension.x * this.position.x + padding
+            val yPos = position.y + dimension.y * this.position.y + padding
             val width = this.dimension.x * dimension.x - (padding * 2)
             val height = this.dimension.y * dimension.y - (padding * 2)
 
@@ -87,7 +136,12 @@ abstract class Treasure(var position: Vector2, val rotate: Boolean) : GameObject
         }
     }
 
-    fun getTreasureTiles(): ArrayList<Vector2> {
+    /**
+     * Gets the position of all the tiles that the treasure span.
+     *
+     * @return tiles: ArrayList<Vector2> - list of grid coordinates
+     */
+    fun getTreasureTiles(): {
         val tiles = ArrayList<Vector2>()
         for (i in 1 until dimension.x.toInt() + 1) {
             val x = position.x.toInt() + i - 1
@@ -99,6 +153,10 @@ abstract class Treasure(var position: Vector2, val rotate: Boolean) : GameObject
         return tiles
     }
 
+    /**
+     * toString specific for Treasures
+     * @return String
+     */
     override fun toString(): String {
         return "Treasure(pos=$position, rotate=$rotate, name='$name')"
     }
