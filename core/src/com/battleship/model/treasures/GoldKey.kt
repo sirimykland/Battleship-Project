44a8c8c72eb81c83f800db4a50b7c8133c3a73fc
Siri Mykland <sirim_mykland@hotmail.com>
2020-04-20 12:52:12 +0200

    doc for treasures Goldcoin, goldkey and treasurechest

--- core/src/com/battleship/model/treasures/GoldKey.kt
+++ core/src/com/battleship/model/treasures/GoldKey.kt
@@ -6,6 +6,13 @@ import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.math.Vector2
 
+/**
+ * Inherits behavior from [Treasure]
+ *
+ * @constructor inherited from [Treasure]:
+ * @property position: Vector2 - position of treasure on board grid
+ * @property rotate: Boolean - describes if treasure is rotated, default: false
+ */
 class GoldKey(position: Vector2, rotate: Boolean) : Treasure(position, rotate) {
     override var dimension: Vector2 = Vector2(1f, 2f)
     override var name: String = "Shiny gold key"
@@ -18,6 +25,9 @@ class GoldKey(position: Vector2, rotate: Boolean) : Treasure(position, rotate) {
         Sprite(Texture("images/treasures/key.png"))
     }
 
+    /**
+     * If rotate is true init block triggers rotateTreasure after constructor setup
+     */
     init {
         if (rotate) rotateTreasure()
     }
--- core/src/com/battleship/model/treasures/TreasureChest.kt
+++ core/src/com/battleship/model/treasures/TreasureChest.kt
@@ -7,12 +7,11 @@ import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.math.Vector2
 
 /**
- * TODO
+ * Inherits behavior from [Treasure]
  *
- * @constructor inherited from [Treasure]
- *
- * @param position
- * @param rotate
+ * @constructor inherited from [Treasure]:
+ * @property position: Vector2 - position of treasure on board grid
+ * @property rotate: Boolean - describes if treasure is rotated, default: false
  */
 class TreasureChest(position: Vector2, rotate: Boolean) : Treasure(position, rotate) {
     override var dimension: Vector2 = Vector2(2f, 2f)
@@ -22,5 +21,8 @@ class TreasureChest(position: Vector2, rotate: Boolean) : Treasure(position, rot
     override var sprite: Sprite = Sprite(Texture("images/treasures/chest.png"))
     override var type: TreasureType = TreasureType.TREASURECHEST
 
+    /**
+     * If rotate is true init block triggers rotateTreasure after constructor setup
+     */
     init { if (rotate) rotateTreasure() }
 }
