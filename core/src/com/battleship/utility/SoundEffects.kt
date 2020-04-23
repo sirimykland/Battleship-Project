31582d8d8c4edc9e85a93464a29961e5adf67b0f
Siri Mykland <sirim_mykland@hotmail.com>
2020-04-23 08:47:17 +0200

    Soundeffects doc

--- core/src/com/battleship/utility/SoundEffects.kt
+++ core/src/com/battleship/utility/SoundEffects.kt
@@ -4,6 +4,9 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.audio.Sound
 import com.battleship.BattleshipGame
 
+/**
+ * Singleton for manging sound effects
+ */
 object SoundEffects {
 
     private val hit: Sound = Gdx.audio.newSound(Gdx.files.internal("audio/hit_sound.mp3"))
@@ -19,26 +22,55 @@ object SoundEffects {
     val GOLDKEY: Sound = Gdx.audio.newSound(Gdx.files.internal("audio/key_sound.mp3"))
     val TREASURECHEST: Sound = Gdx.audio.newSound(Gdx.files.internal("audio/chest_sound.mp3"))
 
+    /**
+     *  Load method
+     */
     fun load() {
         println("SFX Loaded")
     }
 
+    /**
+     * Triggers sound to play on Hit
+     *
+     * @param volume: Float - the volume of the sound played: default is 0.6f
+     */
     fun playHit(volume: Float = 0.8f) {
         play(hit, volume)
     }
 
+    /**
+     * Triggers sound to play on click
+     *
+     * @param volume: Float - the volume of the sound played
+     */
     fun playClick(volume: Float = 4.0f) {
         play(click, volume)
     }
 
+    /**
+     * Triggers sound to play for victory
+     *
+     * @param volume: Float - the volume of the sound played
+     */
     fun playVictory(volume: Float = 0.6f) {
         play(victory, volume)
     }
 
+    /**
+     * Triggers sound to play sound for loosing
+     *
+     * @param volume: Float - the volume of the sound played
+     */
     fun playLosing(volume: Float = 0.6f) {
         play(losing, volume)
     }
 
+    /**
+     * Checks global sound settings to decide whether to play or not
+     *
+     * @param sound: Sound
+     * @param volume: Float
+     */
     fun play(sound: Sound, volume: Float) {
         if (BattleshipGame.soundOn) {
             sound.stop()
@@ -46,6 +78,9 @@ object SoundEffects {
         }
     }
 
+    /**
+     *  Dispose method: for disposing all sound objects
+     */
     fun dispose() {
         listOf(hit, victory, losing, click,
             METALDETECTOR, BIGEQUIPMENT, SHOVEL,
