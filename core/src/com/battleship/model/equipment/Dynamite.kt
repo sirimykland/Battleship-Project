2dd22e063e1e10832e3500c8bc46559b8b70c9ed
henriklarsen <henrifla@stud.ntnu.no>
2020-04-23 10:48:11 +0200

    Renamed to dynamite and removed refreshbutton

--- core/src/com/battleship/model/equipment/Dynamite.kt
+++ core/src/com/battleship/model/equipment/Dynamite.kt
@@ -5,8 +5,8 @@ import com.battleship.utility.SoundEffects
 
 class BigEquipment : Equipment() {
     override var searchRadius: Int = 1
-    override var uses: Int = 10
-    override var name: String = "Big equipment"
+    override var uses: Int = 1
+    override var name: String = "Dynamite"
     override var active: Boolean = false
     override var sound: Sound = SoundEffects.BIGEQUIPMENT
 }
