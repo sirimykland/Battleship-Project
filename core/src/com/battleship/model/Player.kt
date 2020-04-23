7c9845200bd24c0a920a4835666de30c9cafddb9
Siri Mykland <sirim_mykland@hotmail.com>
2020-04-23 08:53:02 +0200

    Player class doc

--- core/src/com/battleship/model/Player.kt
+++ core/src/com/battleship/model/Player.kt
@@ -3,17 +3,30 @@ package com.battleship.model
 import com.battleship.model.equipment.EquipmentSet
 import com.battleship.model.ui.Board
 
+/**
+ * Player class for storing player entities
+ *
+ * @property playerId: String
+ * @property playerName: String
+ */
 class Player(var playerId: String = "", var playerName: String = "") {
     private val boardSize: Int = 10
     var equipmentSet = EquipmentSet()
-    var board: Board =
-        Board(boardSize)
+    var board: Board = Board(boardSize)
     var health: Int = board.getCombinedTreasureHealth()
 
+    /**
+     * Sets health variable to be the combined health
+     * of all treasures on to the players board.
+     */
     fun updateHealth() {
         health = board.getCombinedTreasureHealth()
     }
 
+    /**
+     * toString specific for Player
+     * @return String
+     */
     override fun toString(): String {
         return "Player(Id='$playerId', Name='$playerName', board=$board)"
     }
