58077542822db8f8b578dbfd824859f84b624a92
Siri Mykland <sirim_mykland@hotmail.com>
2020-04-23 10:49:28 +0200

    State class doc

--- core/src/com/battleship/controller/state/State.kt
+++ core/src/com/battleship/controller/state/State.kt
@@ -4,22 +4,42 @@ import com.battleship.controller.firebase.FirebaseController
 import com.battleship.view.View
 
 /**
- * Abstract state class declaring common functions and variables
+ * Abstract State class declaring common functions and variables
  */
-// TODO Siri
 abstract class State(private val controller: FirebaseController) {
     abstract var view: View
 
+    /**
+     * Called once when the State is first initialized.
+     */
     abstract fun create()
 
+    /**
+     * Updates as often as the game renders itself.
+     *
+     * @param dt: Float - delta time since last call
+     */
     abstract fun update(dt: Float)
 
+    /**
+     * Called when the State should render itself.
+     */
     abstract fun render()
 
+    /**
+     * Called when the State is paused, usually when it's not active or visible on-screen.
+     * Is also paused before state is destroyed.
+     */
     open fun pause() {}
 
+    /**
+     * Called when the State is resumed from a paused state, usually when it regains focus.
+     */
     open fun resume() {}
 
+    /**
+     * Called once when the State is destroyed.
+     */
     open fun dispose() {
         view.dispose()
     }
