fe8fc553af517c45145983551d1c861699d11a6e
Siri Mykland <sirim_mykland@hotmail.com>
2020-04-23 10:26:45 +0200

    GSM Class documented

--- core/src/com/battleship/GameStateManager.kt
+++ core/src/com/battleship/GameStateManager.kt
@@ -6,14 +6,23 @@ import com.battleship.model.GameListObject
 import java.util.Stack
 import java.util.UUID
 
+/**
+ * GameStateManager is a Singleton that manages the stack of states
+ * that the game goes through.
+ */
 object GameStateManager {
     var username = ""
-    var userId: String = UUID.randomUUID().toString() //
+    var userId: String = UUID.randomUUID().toString()
     var activeGame: Game? = null
     var pendingGames = ArrayList<GameListObject>()
 
     private val states: Stack<State> = Stack()
 
+    /**
+     * Pushes an item onto the top of the stack.
+     *
+     * @param state: State to push
+     */
     fun push(state: State) {
         if (states.size > 0)
             states.peek().pause()
@@ -21,29 +30,45 @@ object GameStateManager {
         create()
     }
 
-    fun pop() {
-        states.pop().dispose()
-        if (states.size > 0)
-            states.peek().resume()
-    }
-
+    /**
+     * TODO
+     *
+     * @param state
+     */
     fun set(state: State) {
         states.pop().dispose()
         states.push(state)
         create()
     }
 
-    fun create() {
+    /**
+     * Calls the uppermost state in the stack's create method on state initialisation.
+     */
+    private fun create() {
         states.peek().create()
     }
 
+    /**
+     * Called from [BattleshipGame], and calls the uppermost state in the stack' update method.
+     * Updates as often as the game renders itself.
+     *
+     * @param dt: Float - delta time since last call
+     */
     fun update(dt: Float) {
         states.peek().update(dt)
     }
 
+    /**
+     * Called from [BattleshipGame], and calls the uppermost state in the stack' render method.
+     * Runs as often as the game renders itself.
+     */
     fun render() {
         states.peek().render()
     }
+
+    /**
+     * Sets nullable variable activeGame to null
+     */
     fun resetGame() {
         activeGame = null
     }
