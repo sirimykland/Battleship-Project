f84238afebefd87ef350699384a8820bd9a12cab
Mats Tyldum <ma.tyldum@gmail.com>
2020-04-24 10:02:47 +0200

    Linting

--- core/src/com/battleship/view/BasicView.kt
+++ core/src/com/battleship/view/BasicView.kt
@@ -2,9 +2,7 @@ package com.battleship.view
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.g2d.Sprite
 import com.battleship.model.GameObject
-import com.battleship.utility.TextureLibrary
 
 class BasicView : View() {
 
--- core/src/com/battleship/view/PlayView.kt
+++ core/src/com/battleship/view/PlayView.kt
@@ -2,7 +2,6 @@ package com.battleship.view
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.math.Vector2
 import com.battleship.model.GameObject
 import com.battleship.model.equipment.EquipmentSet
@@ -12,7 +11,6 @@ import com.battleship.utility.GdxGraphicsUtil.boardPosition
 import com.battleship.utility.GdxGraphicsUtil.boardWidth
 import com.battleship.utility.GdxGraphicsUtil.equipmentSetPosition
 import com.battleship.utility.GdxGraphicsUtil.equipmentSetSize
-import com.battleship.utility.TextureLibrary
 
 class PlayView : View() {
 
