1e9aa45981c14aef246447c873814ba321ec5876
Siri Mykland <sirim_mykland@hotmail.com>
2020-04-26 12:01:49 +0200

    utility class for ReactangleUtil

--- core/src/com/battleship/utility/RectangleUtil.kt
+++ core/src/com/battleship/utility/RectangleUtil.kt
@@ -3,7 +3,18 @@ package com.battleship.utility
 import com.badlogic.gdx.math.Rectangle
 import com.badlogic.gdx.math.Vector2
 
+/**
+ * Gdx.math.Rectangle utility class
+ */
 object RectangleUtil {
+
+    /**
+     * Returns a rectangle
+     *
+     * @param position: Vector2 - start coordinates on the screen
+     * @param size: Vector2 - width and height of the Rectangle
+     * @return Rectangle
+     */
     fun fromVectors(position: Vector2, size: Vector2): Rectangle {
         return Rectangle(position.x, position.y, size.x, size.y)
     }
